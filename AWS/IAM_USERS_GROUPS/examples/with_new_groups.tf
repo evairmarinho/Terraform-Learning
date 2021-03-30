fb040e61e3ceef1d5513ad64648cbb9e4ca6a7a0
Evair Marinho <evairmarinho@hotmail.com>
2021-03-30 20:43:50 -0300

    Credentials variables

--- AWS/IAM_USERS_GROUPS/examples/with_new_groups.tf
+++ AWS/IAM_USERS_GROUPS/examples/with_new_groups.tf
@@ -2,6 +2,8 @@ module "with_new_groups" {
 
   source = "../../.."
   region = "us-east-1"
+  access_key = "<MY_ACCESS_KEY>"
+  secret_key = "<MY_SECRET_KEY>"
   groups        = ["developers"]
   create_groups = true
   users = {
--- AWS/IAM_USERS_GROUPS/examples/without_new_groups.tf
+++ AWS/IAM_USERS_GROUPS/examples/without_new_groups.tf
@@ -3,6 +3,8 @@ module "without_new_groups" {
 
   source = "../../.."
   region = "us-east-1"
+  access_key = "<MY_ACCESS_KEY>"
+  secret_key = "<MY_SECRET_KEY>"
   create_groups = false
   users = {
       "user1": {
