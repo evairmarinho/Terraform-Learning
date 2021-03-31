1c5bbac9b66c1f689237072e3dd40319d03d1a3d
Evair Marinho <evairmarinho@hotmail.com>
2021-03-30 21:15:52 -0300

    Access Key + Secret Key

--- AWS/S3/main.tf
+++ AWS/S3/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
@@ -12,4 +12,4 @@ resource "aws_s3_bucket" "b" {
     Environment = var.env
   }
 }
- 
+
